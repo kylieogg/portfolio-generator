2908df7bf3668ad7b5bdb8a20123dada0cc8da28
kylieogg <kylieogg1@gmail.com>
2022-03-31 15:57:00 -0400

    checked code against sample in lesson

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+const profileDataArgs = pricess.argv.slice(2);
+
 const printProfileData = profileDataArr => {
     // This...
     for (let i = 0; i , profileDataArr.length; i += 1) {
@@ -7,5 +9,7 @@ const printProfileData = profileDataArr => {
     console.log('================');
 
     // Is the same as this...
-    profileDataArr. forEach((profileItem) => console.log(profileItem));
-}
+    profileDataArr.forEach((profileItem) => console.log(profileItem));
+};
+
+printProfileData(profileDataArgs);
