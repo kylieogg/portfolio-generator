c4bb73b6dfee1ec8b0f40964db5ffde8c70c50e2
kylieogg <kylieogg1@gmail.com>
2022-03-31 17:24:12 -0400

    updated code for fs model

--- app.js
+++ app.js
@@ -1,15 +1,30 @@
-const profileDataArgs = process.argv.slice(2);
+const fs = require('fs');
 
-const printProfileData = profileDataArr => {
-    // This...
-    for (let i = 0; i , profileDataArr.length; i += 1) {
-        console.log(profileDataArr[i]);
-    }
+const profileDataArgs = process.argv.slice(2, process.argv.length);
 
-    console.log('================');
+const [name, github] = profileDataArgs;
 
-    // Is the same as this...
-    profileDataArr.forEach((profileItem) => console.log(profileItem));
+const generatePage = (name, github) => {
+    return `
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Portfolio Demo</title>
+    </head>
+
+    <body>
+        <h1>${name}</h1>
+        <h2><a href="https://github.com/${github}">Github</a></h2>
+    </body>
+    </html>
+    `;
 };
 
-printProfileData(profileDataArgs);
+fs.writeFile('index.html', generatePage(name, github), err => {
+    if (err) throw err;
+
+    console.log('Portfolio complete! Checkout index.html to see the output!');
+});
