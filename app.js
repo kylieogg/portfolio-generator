96bb2f1c4e2060ac9434b2282a9b809fe3dd9132
kylieogg <kylieogg1@gmail.com>
2022-04-03 09:29:39 -0400

    modified HTML template to handle more data

--- app.js
+++ app.js
@@ -4,40 +4,46 @@ const generatePage = require('./src/page-template');
 
 const promptUser = () => {
     return inquirer.prompt([
-        {
-            type: 'input',
-            name: 'name',
-            message: 'What is your name? (Required)',
-            validate: nameInput => {
-                if (nameInput) {
-                    return true;
-                } else {
-                    console.log('Please enter your name!');
-                    return false;
-                }
-            }
-        },
-        {
-            type: 'input',
-            name: 'github',
-            message: 'Enter your GitHub Username (Required)',
-            validate: githubUser => {
-                if (githubUser) {
-                    return true;
-                } else {
-                    console.log ('Please enter your GitHub Username!');
-                    return false;
-                }
-            }
-        },
-        {
-            type: 'input',
-            name: 'about',
-            message: 'Provide some information about yourself: (Required)',
-            when: ({ confirmAbout }) => confirmAbout
+      {
+        type: 'input',
+        name: 'name',
+        message: 'What is your name? (Required)',
+        validate: nameInput => {
+          if (nameInput) {
+            return true;
+          } else {
+            console.log('Please enter your name!');
+            return false;
+          }
         }
+      },
+      {
+        type: 'input',
+        name: 'github',
+        message: 'Enter your GitHub Username (Required)',
+        validate: githubInput => {
+          if (githubInput) {
+            return true;
+          } else {
+            console.log('Please enter your GitHub username!');
+            return false;
+          }
+        }
+      },
+      {
+        type: 'confirm',
+        name: 'confirmAbout',
+        message: 'Would you like to enter some information about yourself for an "About" section?',
+        default: true
+      },
+      {
+        type: 'input',
+        name: 'about',
+        message: 'Provide some information about yourself:',
+        when: ({ confirmAbout }) => confirmAbout
+      }
     ]);
-};
+  };
    
  const promptProject = portfolioData => {
         console.log(`
@@ -124,11 +130,11 @@ const promptUser = () => {
     promptUser()
         .then(promptProject)
         .then(portfolioData => {
-            const pageHTML = generatePage(mockData);
+            const pageHTML = generatePage(portfolioData);
 
-            // fs.writeFile('./index.html', pageHTML, err => {
-                // if (err) throw new Error(err);
+            fs.writeFile('./index.html', pageHTML, err => {
+                if (err) throw new Error(err);
 
-                // console.log('Page created! Check out index.html in this directory to see it!');
-            //});
+                console.log('Page created! Check out index.html in this directory to see it!');
+            });
         });
