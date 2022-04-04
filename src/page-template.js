12172080a08c38fca69b6f8ae70cde0d3f453440
kylieogg <kylieogg1@gmail.com>
2022-04-04 16:00:06 -0400

    cleaned up page-template.js

--- src/page-template.js
+++ src/page-template.js
@@ -80,9 +80,7 @@ module.exports = templateData => {
         <div class="container flex-row justify-space-between align-center py-3">
             <h1 class="page-title text-secondary bg-dark py-2 px-3">${header.name}</h1>
                 <nav class="flex-row">
-                    <a class="ml-2 my-1 px-2 py-1 bg-secondary text-dark" href="https://github.com/${
-                    header.github
-                    }">Github</a>
+                    <a class="ml-2 my-1 px-2 py-1 bg-secondary text-dark" href="https://github.com/${header.github}">Github</a>
                 </nav> 
         </div>
     </header>
